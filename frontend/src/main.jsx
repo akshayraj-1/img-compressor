eeb95887ff19e892ca31b771430fe29f4ca55633
akshayraj-1 <akshayraj4101@gmail.com>
2024-08-04 12:47:25 +0530

    Recat

---
+++ frontend/src/main.jsx
@@ -0,0 +1,17 @@
+import './main.scss';
+import ReactDOM from 'react-dom/client';
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
+import App from './App.jsx';
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<App/>}>
+
+        </Route>
+    )
+)
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <RouterProvider router={router}/>
+);
