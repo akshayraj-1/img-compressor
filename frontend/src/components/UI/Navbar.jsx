76b2c978192cbb7a6e5dc5c2c7da3f5ae075ce31
akshayraj-1 <akshayraj4101@gmail.com>
2024-08-28 00:16:08 +0530

    Modified Navbar.jsx

--- frontend/src/components/UI/Navbar.jsx
+++ frontend/src/components/UI/Navbar.jsx
@@ -1,16 +1,20 @@
-import {Link, NavLink} from "react-router-dom";
+import {Link} from "react-router-dom";
+import _github_icon from "../../assets/images/_github_icon.svg";
 
 function Navbar() {
 
     return (
-        <nav className="fixed top-0 z-[5] flex justify-between items-center w-full px-5 sm:px-8 py-5 bg-secondary shadow-navBar">
+        <nav
+            className="fixed top-0 z-[5] flex justify-between items-center w-full px-5 sm:px-8 py-5 bg-secondary shadow-navBar">
             <Link to="/">
                 <img className="h-[1.1rem]" src="/images/logo.svg" alt="logo"/>
             </Link>
-            {/*<div className="flex items-center gap-4 sm:gap-6 text-sm sm:text-[0.96rem] font-medium">*/}
-            {/*    <NavLink to="/" className={({isActive}) => isActive ? "text-accent" : "text-textPrimary hover:text-accent"}>Home</NavLink>*/}
-            {/*    <NavLink to="/api" className={({isActive}) => isActive ? "text-accent" : "text-textPrimary hover:text-accent"}>Get API</NavLink>*/}
-            {/*</div>*/}
+            <a href="https://github.com/akshayraj-1/ImageCompressor" target="_blank">
+                <img className="h-6 opacity-70 hover:opacity-100 transition-all ease-in duration-300"
+                     src={_github_icon}
+                     alt="github"
+                />
+            </a>
         </nav>
     );
 }
