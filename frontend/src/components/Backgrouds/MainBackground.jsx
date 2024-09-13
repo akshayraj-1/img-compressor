66bc023a8fbcc811e595babec3feeb1b478c2637
akshayraj-1 <akshayraj4101@gmail.com>
2024-09-13 09:55:25 +0530

    Modified MainBackground.jsx

--- frontend/src/components/Backgrouds/MainBackground.jsx
+++ frontend/src/components/Backgrouds/MainBackground.jsx
@@ -5,7 +5,7 @@ function MainBackground({ children, className }) {
 
     return (
         <section className="relative size-full">
-            <div className="absolute z-[1] size-full bg-mainBg blur-3xl bg-center bg-cover bg-no-repeat opacity-5"></div>
+            <div className="absolute z-[1] size-full bg-mainBg blur-3xl bg-center bg-cover bg-no-repeat opacity-[0.05]"></div>
             <div className={cn("absolute z-[2] size-full", className)}>
                 {children}
             </div>
