251c9d21b93d002012748f397e7a23ea1f3ca1c0
akshayraj-1 <akshayraj4101@gmail.com>
2024-09-19 03:32:14 +0530

    Modified vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   envDir: './',
-  envFiles: ['.env', '.env.development'],
+  envFiles: ['.env.development'],
   build: {
     envDir: './',
     envFile: '.env.production',
