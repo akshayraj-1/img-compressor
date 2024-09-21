a9854c732b3d58941a494fc586dcd18ab3090364
akshayraj-1 <akshayraj4101@gmail.com>
2024-09-21 20:53:09 +0530

    Modified useCustomToast.jsx

--- frontend/src/hooks/useCustomToast.jsx
+++ frontend/src/hooks/useCustomToast.jsx
@@ -1,5 +1,31 @@
-import {useCallback, useEffect, useState} from "react";
-import ToastModal from "../components/Modals/ToastModal.jsx";
+import {useEffect, useState} from "react";
+import cn from "../utils/cn.util.js";
+import {AnimatePresence, motion} from "framer-motion";
+import {CheckCheck, CircleAlert} from "lucide-react";
+import PropTypes from "prop-types";
+
+const variants = {
+    initial: {
+        y: 200,
+        opacity: 0,
+    },
+    final: {
+        y: 0,
+        opacity: 1,
+        transition: {
+            duration: 0.9,
+            ease: "anticipate",
+        },
+    },
+    exit: {
+        y: 200,
+        opacity: 0,
+        transition: {
+            duration: 0.9,
+            ease: "anticipate",
+        }
+    }
+};
 
 function useCustomToast() {
 
@@ -7,16 +33,10 @@ function useCustomToast() {
     const [toastMessage, setToastMessage] = useState("");
     const [toastType, setToastType] = useState("error");
 
-    const showToast = useCallback((message, type = "error") => {
-        setToastType(type);
-        setToastMessage(message);
-        setToastVisible(true);
-    }, []);
-
-    const CustomToastModal = () => {
-        return (
-            <ToastModal showToast={toastVisible} message={toastMessage} type={toastType} />
-        );
+    const styles = {
+        parent: cn("absolute z-[50] bottom-5 sm:bottom-10 flex justify-center w-screen px-5 select-none"),
+        body: cn("relative flex gap-2 items-center bg-secondary w-full sm:w-fit px-5 py-3 rounded-md shadow-md", toastType === "error" ? "bg-red-500" : "bg-green-500"),
+        message: cn("sm:text-[0.8rem] text-sm", toastType === "error" || toastType === "success" ? "text-white" : "text-accent"),
     };
 
     useEffect(() => {
@@ -29,7 +49,36 @@ function useCustomToast() {
         return () => clearTimeout(timer);
     }, [toastVisible]);
 
-    return { CustomToastModal, showToast };
+    const showToast = (message, type = "error") => {
+        setToastType(type);
+        setToastMessage(message);
+        setToastVisible(true);
+    };
+
+    const CustomToastModal = () => {
+        return (
+            <AnimatePresence mode="wait">
+                {
+                    toastVisible &&
+                    <div className={styles.parent}>
+                        <motion.div variants={variants} initial="initial" animate="final" exit="exit"
+                                    className={styles.body}>
+                            {toastType === "error" ? <CircleAlert size={13} className={styles.message}/> :
+                                <CheckCheck size={13} className={styles.message}/>}
+                            <p className={styles.message}>{toastMessage}</p>
+                        </motion.div>
+                    </div>
+                }
+            </AnimatePresence>
+        );
+    };
+
+    showToast.propTypes = {
+        message: PropTypes.string.isRequired,
+        type: PropTypes.oneOf(["error", "success", "normal"]),
+    };
+
+    return {CustomToastModal, showToast};
 
 }
 
