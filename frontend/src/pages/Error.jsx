5bf17f8327e9e0f5fa7a2a666fe243c0d12c963c
akshayraj-1 <akshayraj4101@gmail.com>
2024-09-22 16:33:55 +0530

    Modified Error.jsx

--- frontend/src/pages/Error.jsx
+++ frontend/src/pages/Error.jsx
@@ -1,10 +1,10 @@
-import _404 from "../assets/images/_404.svg";
+import img_404 from "../assets/images/img_404.svg";
 import Button from "../components/UI/Button.jsx";
 
 function Error() {
     return (
         <section className="flex flex-col gap-2 justify-center items-center size-full min-h-screen px-5 sm:px-8">
-            <object data={_404} className="h-[52vh] w-full"/>
+            <object data={img_404} className="h-[52vh] w-full"/>
             <p className="text-xl sm:text-3xl font-bold mt-4">Page Not Found</p>
             <span className="text-textSecondary text-sm sm:text-base text-center mx-6">Oops! The page you are looking for does not exist</span>
             <Button
