18aa9cacadc82340fd805fbea3bf64a14fcb5d32
akshayraj-1 <akshayraj4101@gmail.com>
2024-10-15 10:41:03 +0530

    Modified Footer.jsx

--- frontend/src/components/UI/Footer.jsx
+++ frontend/src/components/UI/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     return (
         <footer className="flex flex-col items-center justify-center gap-2 w-screen px-5 sm:px-8 py-10 bg-surfaceVariant">
             <Link to="/">
-                <img className="h-5" src="/images/logo.svg" alt="logo"/>
+                <img className="h-5" src="/images/logo-grayscale.svg" alt="logo"/>
             </Link>
             <p className="text-sm text-textSecondary text-center mt-2">Copyright © 2024. All rights reserved.</p>
             <p className="text-sm text-textSecondary text-center">Version: {version}</p>
